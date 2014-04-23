df0a559a6d9b7c91c18916c6c513036a048dc428
msulima <sulima.mateusz@gmail.com>
2014-04-23 20:06:06 +0200

    Previous questions list

--- frontend/src/main/java/pl/msulima/guesser/controllers/IndexController.java
+++ frontend/src/main/java/pl/msulima/guesser/controllers/IndexController.java
@@ -1,15 +1,17 @@
 package pl.msulima.guesser.controllers;
 
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import pl.msulima.guesser.dao.Answer;
 import pl.msulima.guesser.domain.QuestionsService;
+import pl.msulima.guesser.model.Question;
+
+import java.util.HashMap;
 
 @Controller
 public class IndexController {
@@ -19,11 +21,17 @@ public class IndexController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index() {
-        return new ModelAndView("index", "answer", new Answer());
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("answer", new Answer());
+        model.put("previousQuestions", Lists.newArrayList(new Question(), new Question()));
+        return new ModelAndView("index", model);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public ModelAndView index(@ModelAttribute("answer") Answer answer) {
-        return new ModelAndView("index", "answer", answer);
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("answer", answer);
+        model.put("previousQuestions", Lists.newArrayList(new Question(), new Question()));
+        return new ModelAndView("index", model);
     }
 }
